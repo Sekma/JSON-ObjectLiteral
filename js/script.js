11d9fcb86ff72c401c16cbe36b2fe49f70de5358
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-09-05 21:39:12 +0100

    affichage-tableau

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const nb_gobelin = 10;
 let gobelins = {};
 
@@ -7,18 +8,28 @@ const consonnes=["g","b","l","n"];
 
 
 for(let i=0; i<nb_gobelin; i++){
+
+// nom 
+
   let nom=premiere_lettre[Math.floor(Math.random()*premiere_lettre.length)]+voyelles[Math.floor(Math.random()*voyelles.length)];
   let nbr_syllabe=Math.floor(Math.random()*3)+1;
   while(nbr_syllabe>0){
     nbr_syllabe--;
     nom=nom+consonnes[Math.floor(Math.random()*consonnes.length)]+voyelles[Math.floor(Math.random()*voyelles.length)];
 }
-let gobelin={sexe:1,
-age:2,
-dents:3}
 
+// clé: valeur
+let gobelin={sexe:(Math.floor(Math.random() * (100)) + 1 <= 58) ? "male" : "femelle",
+             age:Math.floor(Math.random()*31)+5,
+             dents:Math.floor(Math.random()*12)+17}
 
 gobelins[nom]=gobelin;
 }
-
 console.log(gobelins);
+
+// affichage
+// ligne 1
+document.querySelector(".case1-1").innerHTML ="11";
+document.querySelector(".case1-2").innerHTML = "12";
+document.querySelector(".case1-3").innerHTML = "13";
+document.querySelector(".case1-4").innerHTML = "14";
